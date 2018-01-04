fc258d5b90f3470db3f15a3f88325144cce4bdcd
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 15:50:45 -0800

    working calculator

---
+++ calculator.py
@@ -0,0 +1,39 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+while True:
+    inp = raw_input("> ")
+    inp_list = inp.split(" ")
+
+    if inp_list[0] == "q":
+        print "quit"
+        break
+
+    elif inp_list[0] == "+":
+        print add(int(inp_list[1]), int(inp_list[2]))
+
+    elif inp_list[0] == "-":
+        print subtract(int(inp_list[1]), int(inp_list[2]))
+
+    elif inp_list[0] == "*":
+        print multiply(int(inp_list[1]), int(inp_list[2]))
+
+    elif inp_list[0] == "/":
+        print divide(int(inp_list[1]), int(inp_list[2]))
+
+    elif inp_list[0] == "square":
+        print square(int(inp_list[1]))
+
+    elif inp_list[0] == "cube":
+        print cube(int(inp_list[1]))
+
+    elif inp_list[0] == "pow":
+        print power(int(inp_list[1]), int(inp_list[2]))
+
+    elif inp_list[0] == "mod":
+        print mod(int(inp_list[1]), int(inp_list[2]))
